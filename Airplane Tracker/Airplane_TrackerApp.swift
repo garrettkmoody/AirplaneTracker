b99fe38cd2cd9f20c7b9442fc2bf10e8d65ae39e
Garrett Moody <garrettkmoody@gmail.com>
2025-10-03 14:11:36 -0700

    Initial Commit

---
+++ Airplane Tracker/Airplane_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airplane_TrackerApp.swift
+//  Airplane Tracker
+//
+//  Created by Garrett Moody on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Airplane_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
